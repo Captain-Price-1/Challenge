d74311d668dafe13f286877c6bae2888036ed6c6
Anas Parwez <anasparvez237@gmail.com>
2022-07-26 17:24:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import Carousel from "react-bootstrap/Carousel";
 import { React, useState } from "react";
 import BootstrapCarousel from "./BootstrapCarousel";
 import BasicExample from "./ProjectsCards";
 import ProjectsData from "./ProjectsData";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import FeedbackForm from "./FeedbackForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Blogs from "./Blogs";
-const slide1 = require("../src/images/react_dev.png");
-const slide2 = require("../src/images/competitive_coder.png");
-const slide3 = require("../src/images/tech-enthusiast.png");
 
 function App() {
   return (
